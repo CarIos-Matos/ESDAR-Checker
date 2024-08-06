cafa63d04f9c0e00d949ff88c1a5dcbece5f2a4a
chrism01 <chrism01@students.zhaw.ch>
2024-08-06 20:29:17 +0200

    updates csv_helper.py script

--- csv_helper.py
+++ csv_helper.py
@@ -17,7 +17,25 @@ def prepare_csv_output(security_check_result):
     return security_check_result
 
 
-def create_csv_file(prepared_output):
+def create_new_csv_file_with_header():
+    # Define the headers for the CSV file
+    headers = ["URL", "MX Record", "SPF Record", "DKIM Record", "DMARC Record"]
+
+    os.makedirs(os.path.dirname(ABSOLUTE_FILE_PATH + OUTPUT_FILENAME), exist_ok=True)
+    # Try open the file and write output, if not working file is opened in OS and has to be closed before
+    try:
+        with open(ABSOLUTE_FILE_PATH + OUTPUT_FILENAME, mode='w', newline='') as file:
+            writer = csv.writer(file, delimiter=",")
+
+            # Write the headers to the CSV file
+            writer.writerow(headers)
+    except:
+        print(
+            "can't write header because %s is open or another error occured." % (ABSOLUTE_FILE_PATH + OUTPUT_FILENAME))
+        sys.exit(1)
+
+
+def write_rows_to_csv(prepared_output):
     """
         Writes URL information to a CSV file.
 
@@ -25,19 +43,14 @@ def create_csv_file(prepared_output):
         url_info_list (list of lists): A list where each element is a String (TODO: check if this is true) containing information about the DNS records.
         """
 
-    filename = "esdar-check_result.csv"
-    # Define the headers for the CSV file
-    headers = ["URL", "MX Record", "SPF Record", "DKIM Record", "DMARC Record"]
+    if not os.path.isfile(ABSOLUTE_FILE_PATH + OUTPUT_FILENAME):
+        os.makedirs(os.path.dirname(ABSOLUTE_FILE_PATH + OUTPUT_FILENAME), exist_ok=True)
+        create_new_csv_file_with_header()
 
-    # Ensure that the Directory exists
-    os.makedirs(os.path.dirname(ABSOLUTE_FILE_PATH + filename), exist_ok=True)
     # Try open the file and write output, if not working file is opened in OS and has to be closed before
     try:
-        with open(ABSOLUTE_FILE_PATH + filename, mode='w', newline='') as file:
+        with open(ABSOLUTE_FILE_PATH + OUTPUT_FILENAME, mode='w', newline='') as file:
             writer = csv.writer(file, delimiter=",")
-
-            # Write the headers to the CSV file
-            writer.writerow(headers)
 
             # Write the URL information to the CSV file
             if len(prepared_output) > 1:
@@ -47,5 +60,25 @@ def create_csv_file(prepared_output):
                 writer.writerow(prepared_output)
     except:
         print(
-            "can't write output because %s is open, close the file and run the script again." % (ABSOLUTE_FILE_PATH + filename))
+            "can't write output because %s is open, close the file and run the script again." % (
+                        ABSOLUTE_FILE_PATH + OUTPUT_FILENAME))
+        sys.exit(1)
+
+
+def write_new_line_to_csv_file(lines_to_add):
+    # Try to open the file and append output
+    if not os.path.isfile(ABSOLUTE_FILE_PATH + OUTPUT_FILENAME):
+        create_new_csv_file_with_header()
+    try:
+        with open(ABSOLUTE_FILE_PATH + OUTPUT_FILENAME, mode='a', newline='') as file:
+            writer = csv.writer(file, delimiter=",")
+            # Append the new URL information to the CSV file
+            if len(lines_to_add) > 1:
+                for line_to_add in lines_to_add:
+                    writer.writerow(line_to_add)
+            else:
+                writer.writerow(lines_to_add)
+    except Exception as e:
+        print(
+            f"Can't append output because {ABSOLUTE_FILE_PATH + OUTPUT_FILENAME} is open, close the file and run the script again.")
         sys.exit(1)
