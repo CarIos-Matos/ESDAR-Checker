aa60a9c48a6cd03b1e398e7ef8b6bcf155057fcd
chrism01 <chrism01@students.zhaw.ch>
2024-08-08 00:48:46 +0200

    Changes absolute to relative file path and adds update function

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 """
 This File is used to setup the needed Configurations which the user has to perform
 
-ABSOLUTE_FILE_PATH: Provide the absolute File Path which you want to use to for the Output File
-E.g: C:/Users/[USERNAME]/PycharmProjects/ESDAR-Checker/resources/output/
+RELATIVE_FILE_PATH: Provide the relative File Path which you want to use to for the Output File
+E.g: resources/output/ or you can also use the absolute filepath C:/Users/[USERNAME]/PycharmProjects/ESDAR-Checker/resources/output/
 
 @author Merlin von der Weide
-@version 1.1.0-beta
-@date 06.08.2024
+@version 1.2.0-beta
+@date 08.08.2024
 """
-ABSOLUTE_FILE_PATH = "ABSOLUTE_FILE_PATH_WITHOUT_NAME"
+RELATIVE_FILE_PATH = "resources/output/"
+
+def update_path(path):
+    global RELATIVE_FILE_PATH
+    RELATIVE_FILE_PATH = path
